5fbd9d2f477e56f71433ba8d9998673fc1fc55be
Igor.tdf <104040266+IgorTDF@users.noreply.github.com>
2022-05-02 18:05:08 -0300

    Initial commit

---
+++ basexaltura.ts
@@ -0,0 +1,14 @@
+let dato1: number = prompt("ingrese la base");
+
+let dato1: number = Number(dato1);
+console.log(dato1);
+
+let dato2: number = prompt("ingrese la altura");
+
+let dato2: number = Number(dato2);
+
+console.log(dato2);
+
+let resultado: number = dato1 * dato2;
+
+console.log("la base es:", resultado);
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+let dato1: number = prompt("ingrese un dato");
+
+let dato1: number = Number(dato1);
+console.log(dato1);
